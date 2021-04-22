b3e7bbecc05909ed1add268c4864066959677d7b
Kiru <kirushanth.ravindran@gmail.com>
2021-04-21 18:08:27 -0700

    further study pt1.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import stories
+
+currentStory = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -10,9 +12,21 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
-    """generate to questions form with word prompts"""
+    """generate drop down menu or story templates"""
+
+    return render_template('home.html', stories=stories)
+
+@app.route('/story')
+def questions():
+    """generate questions form with word prompts"""
     
-    prompts = silly_story.prompts
+    promptId = request.args.get("stories")
+
+    story = [story for story in stories if story.id == promptId]
+
+    currentStory = stories.index(story[0])
+
+    prompts = story[0].prompts
 
     return render_template('questions.html', prompts=prompts)
 
@@ -22,7 +36,7 @@ def results():
     """Displays results as text story from story instance 
     from the homepage form"""
 
-    story = silly_story.generate(request.args)
+    story = stories[currentStory].generate(request.args)
     return render_template('story.html', story=story)
 
 
--- stories.py
+++ stories.py
@@ -18,11 +18,13 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, words, text, title, id):
         """Create story with words and template text."""
 
         self.prompts = words
         self.template = text
+        self.title = title
+        self.id = id
 
     def generate(self, answers):
         """Substitute answers into text."""
@@ -40,7 +42,7 @@ class Story:
 silly_story = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time, in a long-ago {place}, there lived an exceptionally
-       {adjective} {noun}. It loved to {verb} with {plural_noun}."""
+       {adjective} {noun}. It loved to {verb} with {plural_noun}.""", "Silly Story", "silly"
 )
 
 # Here's another --- you should be able to swap in app.py to use this story,
@@ -48,5 +50,7 @@ silly_story = Story(
 
 excited_story = Story(
     ["noun", "verb"],
-    """OMG!! OMG!! I love to {verb} a {noun}!"""
+    """OMG!! OMG!! I love to {verb} a {noun}!""", "Excited Story", "excited"
 )
+
+stories = [silly_story, excited_story]
